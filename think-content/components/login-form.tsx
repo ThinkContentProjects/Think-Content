fec892b92eaf9b206d51dd106d4bb35119d28db8
jnardone <jnardone@calpoly.edu>
2023-02-27 08:58:55 -0800

    added stuff to login page

--- think-content/components/login-form.tsx
+++ think-content/components/login-form.tsx
@@ -41,23 +41,23 @@ export default function Login() {
     }
 
     return (
-        <div className='flex-1 text-xs sm:text-sm flex flex-col justify-center items-center gap-2 sm:gap-4'>
-            <h1 className=' select-none text-2xl sm:text-4xl uppercase'> Log In </h1>
+        <div className='flex flex-col items-center justify-center flex-1 gap-2 text-xs font-semibold text-blue-900 border-gray-400 bg-gray-50 sm:text-sm sm:gap-4'>
+            <h1 className='text-2xl uppercase select-none sm:text-4xl'> Log In </h1>
             {error && <div className='w-full max-w-[40ch] border-rose-400 border text-center border-solid text-rose-400 py-2'>{error}</div>}
             <input type="text" value={email} required onChange={(e) => setEmail(e.target.value)} placeholder='Email Address' className='outline-none duration-300 border-b-2 border-solid border-white focus:border-cyan-300 text-slate-900 p-2 w-full max-w-[40ch]' />
             <input value={password} required onChange={(e) => setPassword(e.target.value)} type="password" placeholder='Password' className='outline-none text-slate-900 p-2 w-full max-w-[40ch] duration-300 border-b-2 border-solid border-white focus:border-cyan-300' />
-            <button disabled={loading} onClick={submitHandler} className='w-full max-w-[40ch] border border-white border-solid uppercase py-2 duration-300 relative after:absolute after:top-0 after:right-full after:bg-white after:z-10 after:w-full after:h-full overflow-hidden hover:after:translate-x-full after:duration-300 hover:text-slate-900'>
-                <h2 className='relative z-20'>
+            <button disabled={loading} onClick={submitHandler} className='text-gray-500 w-full max-w-[40ch] border border-gray-400 border-solid uppercase py-2 duration-300 relative after:absolute after:top-0 after:right-full after:bg-white after:z-10 after:w-full after:h-full overflow-hidden hover:after:translate-x-full after:duration-300 hover:text-slate-900'>
+                <h2 className='relative z-20 text-gray-900'>
                     Log In
                 </h2>
             </button >
-            <button disabled={loading} onClick={googleSignIn} className='w-full max-w-[40ch] border border-white border-solid uppercase py-2 duration-300 relative after:absolute after:top-0 after:right-full after:bg-white after:z-10 after:w-full after:h-full overflow-hidden hover:after:translate-x-full after:duration-300 hover:text-slate-900'> 
+            <button disabled={loading} onClick={googleSignIn} className='text-gray-500 w-full max-w-[40ch] border border-gray-400 border-solid uppercase py-2 duration-300 relative after:absolute after:top-0 after:right-full after:bg-white after:z-10 after:w-full after:h-full overflow-hidden hover:after:translate-x-full after:duration-300 hover:text-slate-900'> 
                 <h2 className='relative z-20'>
                 Log in with Google
                 </h2>
             </button>
-            <Link href="/forgotpassword" className='duration-300 hover:scale-110 cursor-pointer'> Forgot Password? </Link>
-            <Link href="/signup" className='duration-300 hover:scale-110 cursor-pointer'> Login </Link>
+            <Link href="/forgotpassword" className='duration-300 cursor-pointer hover:scale-110'> Forgot Password? </Link>
+            <Link href="/signup" className='duration-300 cursor-pointer hover:scale-110'> Login </Link>
         </div>
     )
 }
--- think-content/pages/login.tsx
+++ think-content/pages/login.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import Login from '../components/login-form'
+import Navbar from '../components/navbar'
+import SideBar from '@/components/sidebar'
+import CommentBox from '@/components/comment-box'
+import Footer from '@/components/link-footer'
 
 export default function login() {
   return (
-    <Login/>
+    <div className='flex flex-col min-h-screen'>
+      <SideBar/>
+      <Navbar/>
+      <div className='flex justify-center flex-grow'>
+        <main className='flex-auto h-screen py-32' id='content'>
+          <Login/>
+        </main>
+      </div>
+      <Footer/>
+    </div>
+
   )
 }
