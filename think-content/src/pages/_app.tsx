4edad44fb13e3142ee069541a6fe1fbeb110868d
jnardone <jnardone@calpoly.edu>
2023-05-09 12:55:08 -0700

    Can load Facebook SDK

--- think-content/src/pages/_app.tsx
+++ think-content/src/pages/_app.tsx
@@ -5,8 +5,53 @@ import theme from "../chakra/theme";
 import { RecoilRoot } from "recoil";
 import Layout from "../components/Layout/Layout";
 import NextNProgress from "nextjs-progressbar";
+import React, { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    FB: any;
+    fbAsyncInit: () => void;
+  }
+}
+
+export {};
+
+const loadFacebookSDK = () => {
+  return new Promise<void>((resolve) => {
+    window.fbAsyncInit = function() {
+      window.FB.init({
+        appId: '205411482261922',
+        cookie: true,
+        xfbml: true,
+        version: 'v15.0'
+      });
+
+      // Additional initialization code or event handlers
+
+      resolve();
+    };
+
+    (function(d, s, id) {
+      let js: HTMLScriptElement,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s) as HTMLScriptElement; // Explicitly type as HTMLScriptElement
+      js.id = id;
+      (js as any).src = 'https://connect.facebook.net/en_US/sdk.js'; // Type assertion for 'src'
+      fjs.parentNode?.insertBefore(js, fjs);
+    })(document, 'script', 'facebook-jssdk');
+  });
+};
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    loadFacebookSDK();
+  }, []);
+
   return (
     // Gives us access to global state everywhere in our app
     <RecoilRoot>
@@ -19,3 +64,4 @@ export default function App({ Component, pageProps }: AppProps) {
     </RecoilRoot>
   );
 }
+
--- think-content/src/pages/index.tsx
+++ think-content/src/pages/index.tsx
@@ -1,7 +1,26 @@
 import useWorkspaceData from "@/src/hooks/useWorkspaceData";
+import { useEffect } from "react";
 
 const Home: React.FC = () => 
 {
+  useEffect(() => {
+    // Call FB.getLoginStatus when the component mounts
+    window.FB.getLoginStatus(function(response: any) {
+      // Handle the response here
+      if (response.status === 'connected') {
+        // The user is logged in and has authorized your app
+        const accessToken = response.authResponse.accessToken;
+        console.log('User is logged in. Access Token:', accessToken);
+      } else if (response.status === 'not_authorized') {
+        // The user is logged in to Facebook, but has not authorized your app
+        console.log('User is logged in but has not authorized the app');
+      } else {
+        // The user is not logged in to Facebook
+        console.log('User is not logged in');
+      }
+    });
+  }, []);
+
   const { workspaceStateValue } = useWorkspaceData();
 
   return <div className="flex flex-col min-h-screen bg-blue-50" />
