1becd95afe9f2a73e654bf2aa18ebfcc6f3709b3
lradovan <lucasradovan@gmail.com>
2023-05-24 14:30:39 -0700

    fixed emails to be more professional

--- think-content/functions/src/index.ts
+++ think-content/functions/src/index.ts
@@ -30,11 +30,29 @@ exports.sendWelcomeEmail = functions.firestore
   .onCreate((snap: any, context: any) => {
     console.log("email id" + snap.data().email);
 
-    let message =
-      "Hello " +
-      (snap.data()?.displayName || snap.data().email?.split("@")[0]) +
-      ",";
-    message += "<br>Thank you for signing up for Think Content!";
+    let name = 
+      (snap.data()?.displayName || snap.data().email?.split("@")[0]);
+
+    let message = `<!DOCTYPE html>
+    <html>
+    <head>
+      <title>Welcome to Think Content</title>
+    </head>
+    <body>
+      <h2>Welcome to Think Content!</h2>
+      <p>Dear ${name},</p>
+      <p>Thank you for signing up for Think Content. We are excited to have you on board!</p>
+      <p>With Think Content, you can collaborate with teams, manage projects, and create amazing content.</p>
+      <p>Should you have any questions or need assistance, please feel free to reach out to our support team.</p>
+
+      <hr style="border:none; border-top:1px solid #e5e5e5; margin:20px 0;" />
+    
+      <p>
+        Best regards,<br />
+        The Think Content Team
+      </p>
+    </body>
+    </html>`
 
     const mailOptions = {
       from: "lucasradovan@gmai.com",
@@ -58,14 +76,31 @@ exports.sendInviteEmail = functions.firestore
   .onCreate((snap: any, context: any) => {
     console.log("email id" + snap.data().email);
 
-    let message = "Hello,";
-    message += `<br>You've been invite to join the workspace "${
+    let message = `<!DOCTYPE html>
+    <html>
+    <head>
+      <title>Email Invitation</title>
+    </head>
+    <body>
+      <p><strong>${snap.data().invitedBy} invited you to the workspace ${
       snap.data().workspaceName
-    }."`;
-    message += "<br>Click on the link to view the invitation: ";
-    message += `<a href="http://localhost:3000/invite/${
-      snap.data().workspaceId
-    }">workspace</a>`;
+    }.</strong></p>
+      <p>Click the button below to view the invitation:</p>
+      <p>
+        <a href="http://localhost:3000/invite/${snap.data().workspaceId}">
+          <button style="padding: 10px 20px; background-color: #915EFF; color: white; border: none; cursor: pointer;">
+            View Invitation
+          </button>
+        </a>
+      </p>
+      <hr style="border:none; border-top:1px solid #e5e5e5; margin:20px 0;" />
+
+      <p>
+        Best regards,<br />
+        The Think Content Team
+      </p>
+    </body>
+    </html>`;
 
     const mailOptions = {
       from: "lucasradovan@gmail.com",
